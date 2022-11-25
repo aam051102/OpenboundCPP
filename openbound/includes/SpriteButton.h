cbb86f01543e80e314a16fd1ad68713fc5f45716
aam051102 <ahlgreenmadsen@gmail.com>
2022-11-25 14:22:28 +0100

    Finish most of SpriteButton.

--- openbound/includes/SpriteButton.h
+++ openbound/includes/SpriteButton.h
@@ -2,17 +2,30 @@
 #define SBURB_SPRITE_BUTTON_H
 
 #include "Common.h"
+#include "Action.h"
+#include "Sprite.h"
 
 namespace SBURB
 {
-    class SpriteButton
+    class SpriteButton : public Sprite
     {
     public:
-        SpriteButton(std::string name, int x, int y, int width, int height, std::string sheet, std::shared_ptr<Action> action);
+        SpriteButton(std::string name, int x, int y, int width, int height, std::string sheetName, std::shared_ptr<Action> action);
         ~SpriteButton();
 
-    protected:
+        void Update();
+        void UpdateMouse();
 
+        void SetState(std::string state);
+
+        std::string Serialize(std::string output);
+
+    protected:
+        std::shared_ptr<AssetTexture> sheet;
+        bool pressed = false;
+        bool mousePressed = false;
+        bool clicked = false;
+        std::shared_ptr<Action> action;
 
     };
 }
---
+++ openbound/src/SpriteButton.cpp
@@ -0,0 +1,91 @@
+#include "SpriteButton.h"
+#include <AssetHandler.h>
+
+namespace SBURB {
+    SpriteButton::SpriteButton(std::string name, int x, int y, int width, int height, std::string sheetName, std::shared_ptr<Action> action) :
+        Sprite(name, x, y, width, height) {
+        this->pressed = false;
+        this->mousePressed = false;
+        this->clicked = false;
+        this->action = action;
+
+        this->sheet = AssetHandler::GetTextureByName(sheetName);
+
+        for (int i = 0; i < (sheet->getSize().x / this->width) * (sheet->getSize().y / this->height); i++) {
+            this->AddAnimation(std::make_shared<Animation>("state" + i, sheetName, 0, 0, width, height, i, 1, "1000"));
+        }
+
+        this->StartAnimation("state0");
+    }
+
+    SpriteButton::~SpriteButton() {
+
+    }
+
+    void SpriteButton::Update() {
+        this->Sprite::Update();
+        this->UpdateMouse();
+    }
+
+    void SpriteButton::UpdateMouse() {
+		int x = Sburb.Mouse.x;
+		int y = Sburb.Mouse.y;
+		int mouseDown = Sburb.Mouse.down;
+
+		this->clicked = false;
+		if (this->HitsPoint(x - this->width / 2, y - this->height / 2)) {
+			Sburb.Stage.style.cursor = "pointer";
+		}
+		if (mouseDown) {
+			if (!this->mousePressed) {
+				this->mousePressed = true;
+				if (this->HitsPoint(x - this->width / 2, y - this->height / 2)) {
+					this->pressed = true;
+				}
+			}
+		}
+		else {
+			if (this->pressed) {
+				if (this->HitsPoint(x - this->width / 2, y - this->height / 2)) {
+					this->clicked = true;
+					std::string nextState = "state" + (stoi(this->animation->GetName().substr(5)) + 1);
+					
+					if (this->animations[nextState]) {
+						this->StartAnimation(nextState);
+					}
+					else {
+						this->StartAnimation("state0");
+					}
+				}
+			}
+		
+			this->pressed = false;
+			this->mousePressed = false;
+		}
+
+		if (this->clicked && this->action) {
+			Sburb.performAction(this->action);
+		}
+    }
+
+	void SpriteButton::SetState(std::string state) {
+		this->StartAnimation("state" + state);
+	}
+
+	std::string SpriteButton::Serialize(std::string output) {
+		output = output + "\n<spritebutton name='" + this->name +
+			(this->x ? "' x='" + std::to_string(this->x) : "") +
+			(this->y ? "' y='" + std::to_string(this->y) : "") +
+			"' width='" + std::to_string(this->width) +
+			"' height='" + std::to_string(this->height) +
+			"' sheet='" + this->animation->GetSheet()->GetName() +
+			"' >";
+
+		if (this->action) {
+			output = this->action->Serialize(output);
+		}
+
+		output = output + "</spritebutton>";
+		return output;
+	}
+}
